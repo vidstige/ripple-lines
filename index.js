13230ca31c4bf5e6cc036555aaecec398b8ef148
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-09-25 21:33:30 +0200

    tripple gaussian for mouintains

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-function gussian(n, height) {
-  const result = [];
-  for (var i = 0; i < n; i++) {
-    result.push(0); // TODO
-  }
-}
+function gaussian(mean, sigma) {
+  return function(x) {
+    return 1 / Math.pow(Math.E, (Math.pow(x - mean, 2)) / (2 * sigma * sigma));
+  };
+};
 
 function draw(canvas, lines) {
   const ctx = canvas.getContext("2d");
   
-  var distance = -100;
+  var distance = -200;
   for (var i = 0; i < lines.length; i++) {
     const line = lines[i];
-    var y = canvas.height + distance;;
+    var y = canvas.height / 2 + distance;
     ctx.moveTo(0, y);
-    for (var j = 0; j < line.length; j++) {
-      ctx.lineTo(j*canvas.width/line.length, y + line[j]);
+    var n = 40;
+    for (var j = 0; j < n; j++) {
+      var nx = j / n - 0.5;
+      ctx.lineTo(
+        j * canvas.width / n,
+        y + line(nx) * -20);
     }
     ctx.stroke();
 
-    distance += 100 / lines.length;
+    distance += 400 / lines.length;
   }
 }
 
-
+function mountains(n) {
+  var parts = [];
+  for (var i = 0; i < n; i++) {
+    parts.push(gaussian((Math.random()-0.5)/2, Math.random() / 10));
+  }
+  return function(x) {
+    var acc = 0;
+    for (var i = 0; i < parts.length; i++) {
+      acc += parts[i](x);
+    }
+    return acc;
+  };
+}
 
 function ready() {
   const canvas = document.getElementById('target');
   var lines = [];
-  lines.push([10, -10, 30]);
-  lines.push([10, -10, 30]);
+  for (var i = 0; i < 20; i++) {
+    //lines.push(gaussian(0, 0.09));
+    lines.push(mountains(3));
+  }
   draw(canvas, lines);  
 }
 
