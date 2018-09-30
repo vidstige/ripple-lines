7e6485027e72a89acf6fd5f80320e910a2280a78
Samuel Carlsson <samuel.carlsson@gmail.com>
2018-09-30 20:36:43 +0200

    glMatrix mode

--- index.js
+++ index.js
@@ -1,58 +1,50 @@
-function gaussian(mean, sigma) {
-  return function(x) {
-    return 1 / Math.pow(Math.E, (Math.pow(x - mean, 2)) / (2 * sigma * sigma));
-  };
-};
-
-function draw(canvas, lines) {
+// camera - camera matrix
+// scene - plane (e.g. z = 0) and height map function f(u, v) = z
+function render(canvas, camera, scene) {
+  // 1. Iterate lines paralell to camera, farthest first
+  //   1.1. Iterate points on line
+  //   1.2. Evaluate heightmap
+  //   1.3. Render line
+  // 2. Profit  
   const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "white";
-  var distance = -200;
-  for (var i = 0; i < lines.length; i++) {
-    const line = lines[i];
-    var y = canvas.height / 2 + distance;
-    ctx.beginPath(); 
-    ctx.moveTo(0, y);
-    var n = 40;
-    for (var j = 0; j < n; j++) {
-      var nx = j / n - 0.5;
-      ctx.lineTo(
-        j * canvas.width / n,
-        y + line(nx) * -20);
-    }
-    ctx.stroke();
-    ctx.lineTo(canvas.width, y);
-    ctx.lineTo(canvas.width, canvas.height);
-    ctx.lineTo(0, canvas.height);
-    ctx.closePath();
-    ctx.fill();
-
-    distance += 400 / lines.length;
+  var projection = mat4.create();
+  mat4.multiply(projection, camera.K, camera.pose);
+  
+  var vertices = [
+    vec3.fromValues(0, 0, 0),
+    vec3.fromValues(1, 0, 0),
+    vec3.fromValues(1, 0, 1),
+    vec3.fromValues(0, 0, 1),
+  ];
+  const r = 3;
+  var screen = vec3.create();
+  for (var i = 0; i < vertices.length; i++) {
+    vec3.transformMat4(screen, vertices[i], projection);
+    ctx.fillRect(
+      canvas.width * (0.5 + screen[0] / screen[2]),
+      canvas.height * (0.5 - screen[1] / screen[2]),
+      r, r);
   }
 }
 
-function mountains(n) {
-  var parts = [];
-  for (var i = 0; i < n; i++) {
-    parts.push(gaussian((Math.random()-0.5)/2, Math.random() / 10));
-  }
-  return function(x) {
-    var acc = 0;
-    for (var i = 0; i < parts.length; i++) {
-      acc += parts[i](x);
-    }
-    return acc;
-  };
+function x2(x, y) {
+  return 1 / (x*x + y*y);
 }
 
 function ready() {
+  var up = vec3.fromValues(0, 1, 0);
+  var eye = vec3.fromValues(0, 0.2, -2);
+  var center = vec3.fromValues(0, 0, 0);
+  var pose = mat4.create();
+  mat4.lookAt(pose, eye, center, up);
+  var K = mat4.create();
+  mat4.perspective(K, 1.3, 640/480, 0.1, 2);
+  console.log(K, pose);
+
   const canvas = document.getElementById('target');
-  var lines = [];
-  for (var i = 0; i < 20; i++) {
-    //lines.push(gaussian(0, 0.09));
-    lines.push(mountains(3));
-  }
-  draw(canvas, lines);  
+  const camera = {K, pose};
+  const scene = {heightmap: x2};
+  render(canvas, camera, scene);
 }
 
 document.addEventListener('DOMContentLoaded', ready);
